a34b9802f8995465f2d4d1f48e44be6f3361b83f
Christian <chryztcode@gmail.com>
2023-02-23 23:39:21 -0600

    add events to index pages and add info to about-us yes

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
 
       <header>
+        <div>
         <nav>
           <img />
             <a href='/'> Home </a>
@@ -24,9 +25,50 @@ export default function Home() {
 
           
         </nav>
+        </div>
+        
       </header>
 
       <main className={styles.main}>
+        <a href=''>
+        <div>
+          <img />
+          <h2> Events in Madrid </h2>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+          the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+          but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        </a>
+
+        <a href=''>
+        <div>
+          <img />
+          <h2> Events in Toluca </h2>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+          the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+          but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        </a>
+
+        <a href=''>
+        <div>
+          <img />
+          <h2> Events in Lima </h2>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+          the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+          but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        </a>
+        
       </main>
 
       <footer className={styles.footer}>
